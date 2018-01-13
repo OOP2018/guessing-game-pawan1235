fb50c438889eb8ab76830e9d14d48d1ae4197894
pawan1235 <pawan1235@hotmail.co.th>
2018-01-13 16:59:12 +0700

    Add GameSolver javadoc

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -1,6 +1,20 @@
 import java.util.Random;
 
+/**
+ * Automatically find the secret to any NumberGame.
+ */
 public class GameSolver {
+	
+	/**
+	 * Play a NumberGame and return the solution.
+	 * The NumberGame object must provide messages (getMessage)
+	 * containing the phrase "too small" if a guess is too small
+	 * and "too large" if a guess is too large, for efficient
+	 * solution.
+	 *
+	 * @param game is the NumberGame to solve
+	 * @return //TODO what does it return?
+	 */
 	public int play(NumberGame game) {
 
 		int guess = game.getUpperBound()/2;
