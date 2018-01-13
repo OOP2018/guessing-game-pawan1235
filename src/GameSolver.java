cf53e983eeea0dffdbc0cd73ab09b13b70384911
pawan1235 <pawan1235@hotmail.co.th>
2018-01-13 08:48:25 +0700

    add GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -1,4 +1,29 @@
+import java.util.Random;
 
 public class GameSolver {
+	public int play(NumberGame game) {
+
+		int guess = game.getUpperBound()/2;
+		int half = game.getUpperBound()/2;
+		int divide = 2;
+		System.out.println(game.getMessage());
+		while (true) {
+			if (!game.guess(guess) && game.getMessage().contains("too small")) {
+				if(half/divide > 1) {
+					divide *= 2;
+				}
+				guess = guess + (half/divide);
+				
+			} else if (!game.guess(guess) && game.getMessage().contains("too large")) {
+				if(half/divide > 1) {
+					guess = guess - (half/divide);
+				}
+				divide *= 2;
+			} else {
+				return guess;
+			}
+		}
+
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@
 public class Main {
 	public static void main(String[] args) {
 		// upper limit for secret number in guessing game
-		int upperBound = 200;
+		int upperBound = 100;
 		NumberGame game = new GuessingGame(upperBound);
 		GameConsole ui = new GameConsole( );
-		int solution = ui.play( game );
+		GameSolver solve = new GameSolver();
+		int solution = solve.play(game);
 		System.out.println("The answer is "+solution +". You have guessed "+game.getCount()+" times");
 	}
 }
