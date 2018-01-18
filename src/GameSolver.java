5f5cf6c0e3cc6b47376cf403463c4ccd9e1155a9
pawan1235 <pawan1235@hotmail.co.th>
2018-01-18 23:21:51 +0700

    fix issue for GameSolver stuck in infinite loop with small upperbound

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -2,39 +2,34 @@
  * Automatically find the secret to any NumberGame.
  */
 public class GameSolver {
-	
+
 	/**
-	 * Play a NumberGame and return the solution.
-	 * The NumberGame object must provide messages (getMessage)
-	 * containing the phrase "too small" if a guess is too small
-	 * and "too large" if a guess is too large, for efficient
-	 * solution.
+	 * Play a NumberGame and return the solution. The NumberGame object must provide
+	 * messages (getMessage) containing the phrase "too small" if a guess is too
+	 * small and "too large" if a guess is too large, for efficient solution.
 	 *
-	 * @param game is the NumberGame to solve
+	 * @param game
+	 *            is the NumberGame to solve
 	 * @return solution of the game
 	 */
 	public int play(NumberGame game) {
 
-		int guess = game.getUpperBound()/2;
-		int half = game.getUpperBound()/2;
-		int divide = 2;
-		System.out.println(game.getMessage());
-		while (!game.guess(guess)) {
+		int max = game.getUpperBound();
+		int min = 1;
+		int guessNum = min + (max - min)/2;
+		while (!game.guess(guessNum)) {
 			if (game.getMessage().contains("too small")) {
-				if(half/divide > 1) {
-					divide *= 2;
-				}
-				guess = guess + (half/divide);
+				min = guessNum + 1;
+				guessNum = min + (max - min)/2;
 				
 			} else if (game.getMessage().contains("too large")) {
-				if(half/divide > 1) {
-					guess = guess - (half/divide);
-				}
-				divide *= 2;
-			} 
+				max = guessNum - 1;
+				guessNum = min + (max - min)/2;
+			}
+			System.out.println(game.getMessage());
+			System.out.println("Your answer ? "+guessNum);
 		}
-		return guess;
-
+		System.out.println(game.getMessage());
+		return guessNum;
 	}
-
 }
